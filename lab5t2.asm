1ad89ff69da76a0473fcbde455c6f6626a033aa7
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-10-27 20:05:23 +0500

    added lab5t2 maybe same as lab5_2

---
+++ lab5t2.asm
@@ -0,0 +1,65 @@
+.model small
+.stack 100h
+.386
+.data
+str1 db "Enter a character: $"
+str2 db "The ASCII code of $"
+str3 db " in HEXA is : $"
+.code
+	main proc 
+		mov ax,@data
+		mov ds,ax
+		mov dx,offset str1
+		
+		mov ah,09h
+		int 21h
+		
+		mov ah,01h
+		int 21h
+		
+		mov bl,al
+		
+		mov dl,10
+		mov ah,02h
+		int 21h
+		mov dl,13
+		mov ah,02h
+		int 21h
+		
+		mov dx,offset str2
+		
+		mov ah,09h
+		int 21h
+		
+		mov dl,bl
+		mov ah,02h
+		int 21h
+		
+		mov dx,offset str3
+		
+		mov ah,09h
+		int 21h
+		
+		
+		mov cx,2
+again:	rol bl,4
+		mov dl,bl
+		and dl,0fh
+		
+		cmp dl,9
+		jbe digit
+		
+		add dl,7d
+		
+digit:	add dl,'0'
+
+		mov ah,02h
+		int 21h
+		
+		dec cx
+		jnz again
+		
+	mov ah,4ch
+	int 21h
+main endp
+end main 
